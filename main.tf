fcbd85bc8eadf74fa5ff9308b3fd23b26431b8eb
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-25 16:14:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami                    = "ami-0b5eea76982371e91"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = "sg-00214fa55b318445c"
+  vpc_security_group_ids = ["sg-00214fa55b318445c"]
   subnet_id              = "subnet-0b846b9948544a5c1"
   tags = {
     Name = "ExampleAppServerInstance"
